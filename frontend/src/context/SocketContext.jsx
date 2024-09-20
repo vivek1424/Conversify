2e8d3945faa61c77f2454c07b6a92f82823bd5af
vivek1424 <48434935+vivek1424@users.noreply.github.com>
2024-09-20 23:56:33 +0530

    Update SocketContext.jsx with the deployed code link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://conversify-2f5r.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
